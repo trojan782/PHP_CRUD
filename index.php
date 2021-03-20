92a703ccde0df6d148c8a0f12ac5b174f0aac647
david jacobs <davidjacobs522@gmail.com>
2021-03-20 01:55:11 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<!-- Bootstrap 5 boiler plate (offline) made with 💛  by TrojanCodes😃-->
+<!-- Make sure you have Bootstrap 5.0 unziped file in the project directory -->
+<?php
+// $pdo = new PDO('mysql:host=127.0.0.1;port=3306;dbname=products_crud','root', '');
+// $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+// $host = "127.0.0.1";
+// $username = "root";
+// $dbname = 'products_crud';
+// $password = '';
+
+// try {
+//     $dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
+//     $pdo = new PDO($dsn, $username, $password);
+//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     echo "Database Connected Successfully ✅";
+// } catch (PDOException $error) {
+//     echo "connection failed!: " . $error->getMessage();
+// }
+// $statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
+// $statement->execute();
+// $products = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+// try {
+//     $conn = new PDO("mysql:host=$serverna me;  dbname=products_crud", $username, $password);
+//     $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     echo "connected successfully";
+// }
+// catch(PDOException $e) {
+//     echo "connection failed: ".$e->getMessage();
+// }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Complete Crud</title>
+    <link rel="stylesheet" href="bootstrap-5.0.0-alpha1-dist/css/bootstrap.min.css">
+    <!--Bootstrap css file -->
+    <link rel="stylesheet" href="bootstrap-5.0.0-alpha1-dist/css/bootstrap.css">
+    <!--Bootstrap css file-->
+    <link rel="stylesheet" href="./style.css">
+    <!--local stylesheet -->
+</head>
+
+<body>
+    <!-- Your code goes in here -->
+    
+
+        <script src="./script.js"></script>
+        <!--local script file-->
+        <script src="bootstrap-5.0.0-alpha1-dist/js/bootstrap.min.js"></script>
+        <!--Bootstrap js file -->
+        <script src="bootstrap-5.0.0-alpha1-dist/js/bootstrap.js"></script>
+        <!--Bootstrap js file -->
+    </div>
+</body>
+
+</html>

f22e83b816900f687dbd2341713b091b065cdd64
david jacobs <davidjacobs522@gmail.com>
2021-03-21 11:56:31 +0100

    changed the connection type

--- connection.php
+++ connection.php
@@ -5,9 +5,16 @@ $password = '';
 $dbname = 'cms';
 
 //create connection
-$conn = mysqli_connect($host, $user, $password, $dbname);
+// $conn = mysqli_connect($host, $user, $password, $dbname);
+
+// //check connection
+// if(!$conn) {
+//     die("connection failed: " . mysqli_connect_error());
+// }
+//database connection
+$mysqli = new mysqli($host, $user, $password, $dbname); 
 
 //check connection
-if(!$conn) {
-    die("connection failed: " . mysqli_connect_error());
+if(!$mysqli) {
+    die("connection failed:" . mysqli_error($mysqli));
 }

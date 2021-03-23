775ab31a9a7b6baacf7475aca848d04a74891376
david jacobs <davidjacobs522@gmail.com>
2021-03-23 07:42:11 +0100

    changed update method

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@ function get_all_data()
     global $conn;
     $result = mysqli_query($conn, "SELECT * FROM posts");
 
-    if(mysqli_num_rows($result) > 0) {
+    if (mysqli_num_rows($result) > 0) {
 
         echo '<h1>All Posts</h1>';
 
@@ -17,14 +17,14 @@ function get_all_data()
             <div class="card" style="width: 18rem;">
   <img src="..." class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title"><a class="text-secondary" href="single.php?id='.$row['id'].'">'.$row['title'].'</a></h5>
+    <h5 class="card-title"><a class="text-secondary" href="single.php?id=' . $row['id'] . '">' . $row['title'] . '</a></h5>
 
-    <p class="card-text">'.htmlspecialchars_decode(substr($row['content'], 0, 100)) . '...</p>
+    <p class="card-text">' . htmlspecialchars_decode(substr($row['content'], 0, 100)) . '...</p>
     
     <div class="btn-group">
-    <a href = "single.php?id=' .$row['id'].'" class="btn btn-outline-primary" role="button" aria-pressed="true">View</a>
+    <a href = "single.php?id=' . $row['id'] . '" class="btn btn-outline-primary" role="button" aria-pressed="true">View</a>
 
-    <a href="update.php?id='.$row['id'].'" class="btn btn-sm btn-outline-secondary" role="button aria-pressed="true">Edit</a>
+    <a href="update.php?id=' . $row['id'] . '" class="btn btn-sm btn-outline-secondary" role="button aria-pressed="true">Edit</a>
 
     </div>
     <small class="text-muted">9 mins</small>
@@ -37,10 +37,10 @@ function get_all_data()
 }
 
 // to insert into the database
-if(isset($_POST['title']) && isset($_POST['content'])) {
+if (isset($_POST['title']) && isset($_POST['content'])) {
 
     //check title and content empty or not
-    if(!empty($_POST['title']) && !empty($_POST['content'])) {
+    if (!empty($_POST['title']) && !empty($_POST['content'])) {
         $title = mysqli_real_escape_string($conn, htmlspecialchars($_POST['title']));
 
         $content = mysqli_real_escape_string($conn, htmlspecialchars($_POST['content']));
@@ -50,23 +50,21 @@ if(isset($_POST['title']) && isset($_POST['content'])) {
         // $check_content = $conn->query($conn, "SELECT 'title' FROM posts WHERE content = '$title'");
 
 
-        if(mysqli_num_rows($check_content) > 0) {
+        if (mysqli_num_rows($check_content) > 0) {
             echo "<h3>This title already exists - please try a different title name</h3>";
-        }
-        else {
+        } else {
             //insert data into database
             $insert_query = mysqli_query($conn, "INSERT INTO posts (title,content) VALUES('$title', '$content')");
-           
+
             //Now check if data has been inserted
-            if($insert_query) {
+            if ($insert_query) {
                 echo "<script>alert('Data inserted');window.location.href = 'index.php'</script>";
                 exit;
             } else {
                 echo "<h3>Data was not inserted!</h3>";
             }
         }
-
-    }else{
+    } else {
         echo "<h4>Please fill all fields</h4>";
     }
 }
@@ -91,24 +89,32 @@ if(isset($_POST['title']) && isset($_POST['content'])) {
 //     }
 // }
 
-function update_get() {
-    // if(isset($_GET['id']) && is_numeric($_GET['id'])) {
-                global $conn;
-                $id = $_GET['id'];
-                $get_id = mysqli_query($conn, "SELECT * FROM posts WHERE id='$id'");
-        
-                if(mysqli_num_rows($get_id) === 1) {
-                    $row = mysqli_fetch_assoc($get_id);
-                    return($row);
-                }
-                global 
-            if(isset($_POST['update'])) {
-                $update_title = $_POST['update_title'];
-                $update_content = $_POST['update_content'];
+function update_get()
+{
+    if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+        global $conn;
+        $id = $_GET['id'];
+        $get_id = mysqli_query($conn, "SELECT * FROM posts WHERE id='$id'");
 
-                $update_query = mysqli_query($conn, "UPDATE posts SET title='$update_title', content='$update_content' WHERE id='$get_id'");
-                if($update_query) {
-                    echo '<h1>Updated successfully!</h1>';
-                }
+        if (mysqli_num_rows($get_id) === 1) {
+            $row = mysqli_fetch_assoc($get_id);
+            return ($row);
+        }
+    }
+    if(isset($_POST['update_title']) && isset($_POST['update_content'])) {
+
+        //check if items are empty
+        if(!empty($_POST['update_title'])&& !empty($_POST['update_content'])) {
+            $title = mysqli_real_escape_string($conn, htmlspecialchars($_POST['update_title']));
+            $content =mysqli_real_escape_string($conn, htmlspecialchars($_POST['update_content']));
+
+            $update_query = mysqli_query($conn, "UPDATE posts SET title='$title', content='$content' WHERE id='$id'");
+            if($update_query) {
+                echo '<h2>Updated successfully!</h2>';
             }
+        }
+        else {
+            echo '<h1>Please fill in all fields!</h1>';
+        }
+    }
 }
